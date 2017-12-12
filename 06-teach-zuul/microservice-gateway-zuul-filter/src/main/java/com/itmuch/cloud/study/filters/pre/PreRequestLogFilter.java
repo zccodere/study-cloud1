e842fbfcfb7710a420203aa26d88f4d25fc7cf48
zccodere <zccoder@aliyun.com>
2017-12-12 22:30:13 +0800

    新增-第八章《使用Zuul构建微服务网关》教学源码

---
+++ 06-teach-zuul/microservice-gateway-zuul-filter/src/main/java/com/itmuch/cloud/study/filters/pre/PreRequestLogFilter.java
@@ -0,0 +1,36 @@
+package com.itmuch.cloud.study.filters.pre;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+public class PreRequestLogFilter extends ZuulFilter {
+  private static final Logger LOGGER = LoggerFactory.getLogger(PreRequestLogFilter.class);
+
+  @Override
+  public String filterType() {
+    return "pre";
+  }
+
+  @Override
+  public int filterOrder() {
+    return 1;
+  }
+
+  @Override
+  public boolean shouldFilter() {
+    return true;
+  }
+
+  @Override
+  public Object run() {
+    RequestContext ctx = RequestContext.getCurrentContext();
+    HttpServletRequest request = ctx.getRequest();
+    PreRequestLogFilter.LOGGER.info(String.format("send %s request to %s", request.getMethod(), request.getRequestURL().toString()));
+    return null;
+  }
+}
